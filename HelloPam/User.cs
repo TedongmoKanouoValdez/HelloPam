9064892e5092a13b5c673fd945f0f9e69086d45f
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-12-12 03:13:25 +0100

    Ajoutez des fichiers projet.

---
+++ HelloPam/User.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloPam
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public int Profile { get; set; }
+        public bool Status { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User(int id, string username, string password,string fullname, int profile, bool status, byte[] picture, DateTime createdAt)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Profile = profile;
+            Status = status;
+            Picture = picture;
+            CreatedAt = createdAt;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is User user && Id==user.Id;
+        }
+
+    }
+}
